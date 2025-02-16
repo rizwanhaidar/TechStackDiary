5ea96409d7ae41a12b20a8a7ef061c898c9b5f10
Rizwan Haidar <rizwanhaidar@outlook.com>
2025-02-17 01:25:04 +0500

    retailDataEngineering
    Date: Feb 16, 2025. Initial Startup with Dataset Creation MySQL tables creation.

---
+++ data-engineering/DE_projects/2024_p1_retail-data-engineering/scripts/data_generation/Src_data_Creation/main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import uuid
+from faker import Faker
+#from sqlalchemy import create_engine
+
+# Initialize Faker
+fake = Faker()
+
+# Function to generate customer data
+def generate_customers(num_records):
+    customers = []
+    for _ in range(num_records):
+        customers.append({
+            "customer_id": str(uuid.uuid4()),  # Unique UUID for each customer
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "email": fake.email(),
+            "phone": fake.phone_number(),
+            "address": fake.address().replace("\n", ", "),
+            "loyalty_points": fake.random_int(min=0, max=1000)  # Random loyalty points
+        })
+    return pd.DataFrame(customers)
+
+# Generate 100,000 customer records
+customers_df = generate_customers(100000)
+
+# Save to CSV (optional)
+customers_df.to_csv("customers.csv", index=False)
+#
+# # Insert into MySQL
+# # Replace with your MySQL credentials
+# engine = create_engine("mysql+pymysql://user:password@localhost/retail_db")
+# customers_df.to_sql("customers", con=engine, if_exists="replace", index=False)
+#
+# print("Customer data generated and inserted into MySQL!")
---
+++ data-engineering/DE_projects/2024_p1_retail-data-engineering/sql/ddl/01_CRM_System_Custtomer_DDL.sql
@@ -0,0 +1,19 @@
+-- CREATE DATABASE CRM_System;
+-- USE CRM_System;
+-- USER rizwanhaidar
+-- Pass ****123
+
+CREATE TABLE CRM_System.customers (
+    customer_id VARCHAR(36) PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    address VARCHAR(255),
+    loyalty_points INT
+);
+
+SELECT * FROM customers ;
+
+SELECT count(*) FROM customers ;
+
