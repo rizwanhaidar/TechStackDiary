6b7b3f7dfa0388859afaa3235cf2b38f0d8af269
Rizwan Haidar <rizwanhaidar@outlook.com>
2025-02-20 00:21:02 +0500

    Updates to the SRC Data Mapping Sheet

--- data-engineering/DE_projects/2024_p1_retail-data-engineering/scripts/data_generation/Src_data_Creation/write_to_mySQL.py
+++ data-engineering/DE_projects/2024_p1_retail-data-engineering/scripts/data_generation/Src_data_Creation/write_to_mySQL.py
@@ -2,20 +2,20 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 # Load your CSV data into a DataFrame
-#customers_df = pd.read_csv("customers.csv")
+customers_df = pd.read_csv("customers.csv")
 products_df = pd.read_csv("products.csv")
 
-# # Define your MySQL database credentials
-# username = 'rizwanhaidar'
-# password = 'qaws123'
-# host = 'localhost'
-# database = 'CRM_System'
-#
-# # Create a SQLAlchemy engine
-# engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{database}")
+# Define your MySQL database credentials
+username = 'rizwanhaidar'
+password = 'qaws123'
+host = 'localhost'
+database = 'CRM_System'
+
+# Create a SQLAlchemy engine
+engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{database}")
 
 # Write the DataFrame to a MySQL table named 'customers'
-#customers_df.to_sql('customers', con=engine, if_exists='replace', index=False)
+customers_df.to_sql('customers', con=engine, if_exists='replace', index=False)
 
 # Define your MySQL database credentials
 username = 'rizwanhaidar'
