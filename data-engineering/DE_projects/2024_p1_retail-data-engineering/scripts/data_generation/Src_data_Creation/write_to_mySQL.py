4d9d56b80d7fb07dd09d0e424dd237f62d570072
Rizwan Haidar <rizwanhaidar@outlook.com>
2025-02-19 19:49:14 +0500

    Data insertion to the Database MySQl

--- data-engineering/DE_projects/2024_p1_retail-data-engineering/scripts/data_generation/Src_data_Creation/write_to_mySQL.py
+++ data-engineering/DE_projects/2024_p1_retail-data-engineering/scripts/data_generation/Src_data_Creation/write_to_mySQL.py
@@ -2,18 +2,31 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 # Load your CSV data into a DataFrame
-customers_df = pd.read_csv("customers.csv")
+#customers_df = pd.read_csv("customers.csv")
+products_df = pd.read_csv("products.csv")
+
+# # Define your MySQL database credentials
+# username = 'rizwanhaidar'
+# password = 'qaws123'
+# host = 'localhost'
+# database = 'CRM_System'
+#
+# # Create a SQLAlchemy engine
+# engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{database}")
+
+# Write the DataFrame to a MySQL table named 'customers'
+#customers_df.to_sql('customers', con=engine, if_exists='replace', index=False)
 
 # Define your MySQL database credentials
 username = 'rizwanhaidar'
 password = 'qaws123'
 host = 'localhost'
-database = 'CRM_System'
+database = 'Inventory_mgmt_sys'
 
 # Create a SQLAlchemy engine
 engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}/{database}")
 
-# Write the DataFrame to a MySQL table named 'customers'
-customers_df.to_sql('customers', con=engine, if_exists='replace', index=False)
+
+products_df.to_sql('products', con=engine, if_exists='replace', index=False)
 
 print("Customer data inserted into MySQL successfully!")
---
+++ data-engineering/DE_projects/2024_p1_retail-data-engineering/sql/ddl/02_INV_mngmnt_sys_DDL.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE Inventory_mgmt_sys;
+USE Inventory_mgmt_sys;
+-- USER rizwanhaidar
+-- Pass ****123
+TRUNCATE TABLE Inventory_mgmt_sys.products;
+
+CREATE TABLE products (
+    product_id CHAR(36) PRIMARY KEY,  -- UUID stored as CHAR(36)
+    product_name VARCHAR(255) NOT NULL,
+    category ENUM('Electronics', 'Clothing', 'Home & Kitchen', 'Books', 'Toys') NOT NULL,
+    brand VARCHAR(255),
+    unit_price DECIMAL(10,2) NOT NULL,
+    cost_price DECIMAL(10,2) NOT NULL,
+    supplier_id CHAR(36) NOT NULL,  -- UUID stored as CHAR(36)
+    sku VARCHAR(50) UNIQUE NOT NULL,
+    description TEXT,
+    manufacturer VARCHAR(255),
+    weight DECIMAL(5,2) NOT NULL,  -- Weight in kg with 2 decimal places
+    dimensions VARCHAR(50),  -- Format: WxHxD cm
+    is_active BOOLEAN DEFAULT TRUE,
+    CONSTRAINT chk_price CHECK (cost_price <= unit_price)  -- Ensures cost price is not higher than unit price
+);
+
+
+
+SELECT * FROM products ;
+
+SELECT count(*) FROM products ;
+
+SELECT * FROM products LIMIT 10;
+
+
+
